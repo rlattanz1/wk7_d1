179c50431d8572591aa55986adef020d77cb3603
LetsGitFunky <NickHeinBass@gmail.com>
2023-05-08 16:25:56 -0700

    Sessions Controller

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
-    
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create
+        @user = User.new(params[:id])
+        if @user.save
+            render :new
+        else
+            redirect_to new_user_url
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password_digest, :session_token)
+    end
+
 end
--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
     attr_reader :password
     before_validation :ensure_session_token
 
-    def self.find_by_credentials
+    def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
         if user && user.is_valid_password?(password)
             return user
