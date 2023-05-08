0348a995b4aae882538498bc6c72fd5d6ceaf1a6
LetsGitFunky <NickHeinBass@gmail.com>
2023-05-08 15:33:54 -0700

    User Model

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -17,6 +17,35 @@ class User < ApplicationRecord
     attr_reader :password
     before_validation :ensure_session_token
 
-    
+    def self.find_by_credentials
+        user = User.find_by(username: username)
+        if user && user.is_valid_password?(password)
+            return user
+        else
+            return nil
+        end
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_valid_password?(password)
+        password_object = BCrypt::Password.new(password_digest)
+        password_object.is_password?(password)
+    end
+
+    def reset_session_token
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
+
+    # has many....
 
 end
--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/db/migrate/20230508215446_create_users.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/db/migrate/20230508215446_create_users.rb
@@ -1,10 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :username, null: false, index: true, unique: true
+      t.string :username, null: false #index: true, unique: true
       t.string :password_digest, null: false
-      t.string :session_token, null: false, index: true, unique: true
+      t.string :session_token, null: false #index: true, unique: true
       t.timestamps
     end
+    # add_index :username, :session_token, index: true, unique: true
   end
 end
